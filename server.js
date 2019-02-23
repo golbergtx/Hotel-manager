eca06bfe1cc118e43e26fba12b732933849ada48
Danil <golbergdon@gmail.com>
2019-02-23 12:31:52 +0200

    add base connection to database

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+// express App
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
-	res.send('hello world');
+//modules
+const hbs = require('hbs');
+const mysql = require('mysql');
+
+//app config
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'hotel'
 });
 
+app.get('/', (req, res) => {
+	res.render("home");
+});
+
+connection.connect(err => {
+	if (err) throw err;
+	console.log("connected to database");
+});
 app.listen(3000);
