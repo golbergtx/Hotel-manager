f276f4b72fc7870c2cfafdc7ff9cf5c1c2a4b3c0
Danil Motyckiy <d.motyckiy@viseven.com>
2019-06-04 08:06:17 +0300

    modify (guest page): refactoring

--- public/js/guest-page.js
+++ public/js/guest-page.js
@@ -1,5 +1,8 @@
 import Guest from "./data/Guest.js";
 import DateFormater from "./data/Date-formater.js";
+import infoPopup from "./scripts/info-popup.js"
+
+Vue.component('info-popup', infoPopup);
 
 new Vue({
 	el: "article",
@@ -8,7 +11,8 @@ new Vue({
 		displayedGuests: [],
 		nameFilter: "",
 		editGuestIndex: null,
-		guest: {
+		isEditGuestMode: false,
+		editGuestData: {
 			firstName: "",
 			lastName: "",
 			phone: "",
@@ -18,7 +22,7 @@ new Vue({
 			discountCode: ""
 		},
 		openedGuestPopup: false,
-		guestPopupCaption: "",
+		popupHeader: "",
 	},
 	watch: {
 		nameFilter(val) {
@@ -28,82 +32,121 @@ new Vue({
 	},
 	methods: {
 		createGuest() {
-			this.openGuestPopup("create");
+			this.popupHeader = "Додати гостя:";
+			this.isEditGuestMode = false;
+			this.openGuestPopup();
 		},
 		editGuest(index) {
-			this.openGuestPopup("edit", index);
+			this.popupHeader = "Редагувати гостя:";
+			this.isEditGuestMode = true;
+			this.editGuestIndex = index;
+			this.editGuestData = Object.assign({}, this.displayedGuests[index]);
+			this.editGuestData.dateOfBirth = DateFormater.getFormatDate(this.editGuestData.dateOfBirth, "-", true);
+			this.openGuestPopup(index);
 		},
 		deleteGuest(index) {
-			this.sendGuestData("delete-guest", {guestID: this.displayedGuests[index].id}, () => {
-				this.displayedGuests.splice(index, 1);
-			})
+			this.sendData("delete-guest", JSON.stringify({guestID: this.displayedGuests[index].id}), () => {
+					this.displayedGuests.splice(index, 1);
+				}
+			)
 		},
-		openGuestPopup(type, index) {
-			this.resetGuest();
-			this.editType = type;
-			this.editGuestIndex = index;
-			if (this.editType === "edit") {
-				this.guestPopupCaption = "Редактировать гостя:";
-				this.guest = Object.assign({}, this.displayedGuests[index]);
-				this.guest.dateOfBirth = DateFormater.getFormatDate(this.guest.dateOfBirth, "-", true)
-			} else if (this.editType === "create") {
-				this.guestPopupCaption = "Добавить гостя:";
-			}
+		
+		openGuestPopup(index) {
 			this.openedGuestPopup = true;
 		},
 		closeGuestPopup() {
+			this.resetGuest();
 			this.openedGuestPopup = false;
 		},
 		resetGuest() {
-			for (let key in this.guest) {
-				this.guest[key] = "";
+			for (let key in this.editGuestData) {
+				this.editGuestData[key] = "";
 			}
 		},
+		
 		saveGuestData() {
-			if (this.editType === "edit") {
-				this.sendGuestData("update-guest", this.guest, () => {
-					this.displayedGuests[this.editGuestIndex].firstName = this.guest.firstName;
-					this.displayedGuests[this.editGuestIndex].lastName = this.guest.lastName;
-					this.displayedGuests[this.editGuestIndex].phone = this.guest.phone;
-					this.displayedGuests[this.editGuestIndex].address = this.guest.address;
-					this.displayedGuests[this.editGuestIndex].passportDetails = this.guest.passportDetails;
-					this.displayedGuests[this.editGuestIndex].dateOfBirth = new Date(this.guest.dateOfBirth);
-					this.displayedGuests[this.editGuestIndex].discountCode = this.guest.discountCode;
-				});
-			} else if (this.editType === "create") {
-				this.sendGuestData("add-guest", this.guest, (result) => {
-					this.guests.push(new Guest(JSON.parse(result).id,
-						this.guest.firstName,
-						this.guest.lastName,
-						this.guest.phone,
-						this.guest.address,
-						this.guest.passportDetails,
-						new Date(this.guest.dateOfBirth),
-						this.guest.discountCode
-					));
-				});
+			if (!this.isValidEditGuestData()) {
+				alert("Заповніть всі поля!");
+				return
 			}
-			this.closeGuestPopup();
+			if (this.isEditGuestMode) {
+				this.updateGuest();
+			}
+			else {
+				this.addGuest()
+			}
 		},
-		sendGuestData(action, data, callback) {
+		
+		isValidEditGuestData() {
+			return this.editGuestData.firstName !== "" &
+				this.editGuestData.lastName !== "" &
+				this.editGuestData.lastName !== "" &
+				this.editGuestData.phone !== "" &
+				this.editGuestData.address !== "" &
+				this.editGuestData.passportDetails !== "" &
+				this.editGuestData.dateOfBirth !== "";
+		},
+		
+		addGuest() {
+			this.sendData("add-guest", JSON.stringify(this.editGuestData), (response, status) => {
+				if (status === 200) {
+					this.guests.push(new Guest(JSON.parse(response).id,
+						this.editGuestData.firstName,
+						this.editGuestData.lastName,
+						this.editGuestData.phone,
+						this.editGuestData.address,
+						this.editGuestData.passportDetails,
+						new Date(this.editGuestData.dateOfBirth),
+						this.editGuestData.discountCode
+					));
+					
+					this.$refs.infoPopup.openInfoPopup("Додано!");
+					this.closeGuestPopup();
+				} else {
+					this.$refs.infoPopup.openInfoPopup("Oops! Something went wrong");
+				}
+			});
+		},
+		updateGuest() {
+			this.sendData("update-guest", JSON.stringify(this.editGuestData), (response, status) => {
+				if (status === 200) {
+					const guest = this.displayedGuests[this.editGuestIndex];
+					guest.firstName = this.editGuestData.firstName;
+					guest.lastName = this.editGuestData.lastName;
+					guest.phone = this.editGuestData.phone;
+					guest.address = this.editGuestData.address;
+					guest.passportDetails = this.editGuestData.passportDetails;
+					guest.dateOfBirth = new Date(this.editGuestData.dateOfBirth);
+					guest.discountCode = this.editGuestData.discountCode;
+					
+					this.$refs.infoPopup.openInfoPopup("Оновлено!");
+					this.closeGuestPopup();
+				} else {
+					this.$refs.infoPopup.openInfoPopup("Oops! Something went wrong");
+				}
+			});
+		},
+		
+		sendData(action, data, callback) {
 			const xhr = new XMLHttpRequest();
 			xhr.open("POST", action);
 			xhr.setRequestHeader("Content-Type", "application/json");
-			xhr.send(JSON.stringify(data));
+			xhr.send(data);
 			xhr.onloadend = () => {
-				if (xhr.status === 200) callback && callback(xhr.response);
+				if (callback) callback(xhr.response, xhr.status);
 			}
 		},
 		getGuestsData() {
 			const xhr = new XMLHttpRequest();
 			xhr.open("POST", "get-guests", false);
-			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 			xhr.send();
 			if (xhr.status === 200) {
 				return JSON.parse(xhr.response, (key, value) => {
 					if (key.startsWith("date")) return new Date(value);
 					return value
 				});
+			} else {
+				alert("Oops! Something went wrong");
 			}
 		}
 	},
